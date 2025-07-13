2a895e9cfcdaa2df00276ba2d6a2697c6d195866
Rushi1910 <rushiofficial92@gmail.com>
2025-07-13 11:10:11 +0530

    Fix:use single Expree app instance

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
-import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import express from "express";
 
 import path from "path";
 
